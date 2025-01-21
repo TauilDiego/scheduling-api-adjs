7a0b55a16efe969f7b8fe55b8c0c41fd2391ca38
Macbook pro de Diego <tauildiego@icloud.com>
2025-01-21 20:41:03 -0300

    feat: implementing basic user creation and deletion with authentication

--- start/routes.ts
+++ start/routes.ts
@@ -7,10 +7,41 @@
 |
 */
 
+import User from '#models/user'
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
   return {
     hello: 'world',
+  }
+})
+
+router.post('users/:id/tokens', async ({ params }) => {
+  const user = await User.findOrFail(params.id)
+  const token = await User.accessTokens.create(user)
+
+  return {
+    type: 'bearer',
+    value: token.value!.release
+  }
+})
+
+router.post('user', async ({ request }) => {
+  console.log(request.all())
+  const user = await User.create(request.all())
+
+  const token = await User.accessTokens.create(user)
+  return {
+    id: user.id,
+    createdAt: user.createdAt,
+    token: token
+  }
+})
+
+router.delete('user/:id', async ({ params }) => {
+  const user = await User.findOrFail(params.id)
+  await user.delete()
+  return {
+    message: 'User deleted'
   }
 })
